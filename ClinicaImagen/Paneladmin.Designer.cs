19340aa21c7d9556732223bc1a72fb22b88c5315
Federico Zednicek <fedecampeon2010@hotmail.com>
2022-11-13 13:57:26 -0300

    Cambios del diseño

--- ClinicaImagen/Paneladmin.Designer.cs
+++ ClinicaImagen/Paneladmin.Designer.cs
@@ -40,9 +40,17 @@
             this.lblCorreo = new System.Windows.Forms.Label();
             this.btnResetearPwd = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
-            this.btnGraficas = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label4 = new System.Windows.Forms.Label();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.dgVerificados)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dgFormularios)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // dgVerificados
@@ -53,12 +61,12 @@
             this.dgVerificados.AllowUserToResizeRows = false;
             this.dgVerificados.BackgroundColor = System.Drawing.Color.CornflowerBlue;
             this.dgVerificados.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgVerificados.Location = new System.Drawing.Point(-1, 32);
+            this.dgVerificados.Location = new System.Drawing.Point(1, 121);
             this.dgVerificados.MultiSelect = false;
             this.dgVerificados.Name = "dgVerificados";
             this.dgVerificados.ReadOnly = true;
             this.dgVerificados.RowTemplate.Height = 25;
-            this.dgVerificados.Size = new System.Drawing.Size(397, 421);
+            this.dgVerificados.Size = new System.Drawing.Size(140, 421);
             this.dgVerificados.TabIndex = 0;
             this.dgVerificados.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgVerificados_CellContentClick);
             // 
@@ -70,21 +78,22 @@
             this.dgFormularios.AllowUserToResizeRows = false;
             this.dgFormularios.BackgroundColor = System.Drawing.Color.CornflowerBlue;
             this.dgFormularios.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgFormularios.Location = new System.Drawing.Point(398, 32);
+            this.dgFormularios.Location = new System.Drawing.Point(468, 121);
             this.dgFormularios.MultiSelect = false;
             this.dgFormularios.Name = "dgFormularios";
             this.dgFormularios.ReadOnly = true;
             this.dgFormularios.RowTemplate.Height = 25;
-            this.dgFormularios.Size = new System.Drawing.Size(402, 421);
+            this.dgFormularios.Size = new System.Drawing.Size(512, 421);
             this.dgFormularios.TabIndex = 1;
             this.dgFormularios.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick_1);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
             this.label1.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.label1.Location = new System.Drawing.Point(-1, 6);
+            this.label1.Location = new System.Drawing.Point(1, 96);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(112, 25);
             this.label1.TabIndex = 2;
@@ -96,7 +105,7 @@
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Segoe UI Semibold", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
             this.label2.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.label2.Location = new System.Drawing.Point(398, 6);
+            this.label2.Location = new System.Drawing.Point(408, 96);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(120, 25);
             this.label2.TabIndex = 3;
@@ -106,9 +115,9 @@
             // 
             this.btnVerificar.BackColor = System.Drawing.Color.GreenYellow;
             this.btnVerificar.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
-            this.btnVerificar.Location = new System.Drawing.Point(509, 459);
+            this.btnVerificar.Location = new System.Drawing.Point(143, 552);
             this.btnVerificar.Name = "btnVerificar";
-            this.btnVerificar.Size = new System.Drawing.Size(172, 47);
+            this.btnVerificar.Size = new System.Drawing.Size(140, 38);
             this.btnVerificar.TabIndex = 4;
             this.btnVerificar.Text = "Verificar";
             this.btnVerificar.UseVisualStyleBackColor = false;
@@ -118,9 +127,9 @@
             // 
             this.btnActualizar.BackColor = System.Drawing.Color.BurlyWood;
             this.btnActualizar.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
-            this.btnActualizar.Location = new System.Drawing.Point(115, 459);
+            this.btnActualizar.Location = new System.Drawing.Point(12, 552);
             this.btnActualizar.Name = "btnActualizar";
-            this.btnActualizar.Size = new System.Drawing.Size(172, 47);
+            this.btnActualizar.Size = new System.Drawing.Size(125, 38);
             this.btnActualizar.TabIndex = 5;
             this.btnActualizar.Text = "Actualizar";
             this.btnActualizar.UseVisualStyleBackColor = false;
@@ -129,7 +138,8 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(836, 41);
+            this.label3.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label3.Location = new System.Drawing.Point(995, 19);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(130, 15);
             this.label3.TabIndex = 6;
@@ -138,7 +148,8 @@
             // lblUsuario
             // 
             this.lblUsuario.AutoSize = true;
-            this.lblUsuario.Location = new System.Drawing.Point(815, 75);
+            this.lblUsuario.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.lblUsuario.Location = new System.Drawing.Point(956, 43);
             this.lblUsuario.Name = "lblUsuario";
             this.lblUsuario.Size = new System.Drawing.Size(50, 15);
             this.lblUsuario.TabIndex = 7;
@@ -147,7 +158,8 @@
             // lblCorreo
             // 
             this.lblCorreo.AutoSize = true;
-            this.lblCorreo.Location = new System.Drawing.Point(815, 110);
+            this.lblCorreo.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.lblCorreo.Location = new System.Drawing.Point(956, 58);
             this.lblCorreo.Name = "lblCorreo";
             this.lblCorreo.Size = new System.Drawing.Size(46, 15);
             this.lblCorreo.TabIndex = 8;
@@ -157,9 +169,9 @@
             // 
             this.btnResetearPwd.BackColor = System.Drawing.Color.LightCoral;
             this.btnResetearPwd.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
-            this.btnResetearPwd.Location = new System.Drawing.Point(808, 173);
+            this.btnResetearPwd.Location = new System.Drawing.Point(85, 596);
             this.btnResetearPwd.Name = "btnResetearPwd";
-            this.btnResetearPwd.Size = new System.Drawing.Size(172, 47);
+            this.btnResetearPwd.Size = new System.Drawing.Size(120, 40);
             this.btnResetearPwd.TabIndex = 9;
             this.btnResetearPwd.Text = "Resetear contraseña";
             this.btnResetearPwd.UseVisualStyleBackColor = false;
@@ -169,33 +181,81 @@
             // 
             this.button1.BackColor = System.Drawing.Color.GreenYellow;
             this.button1.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
-            this.button1.Location = new System.Drawing.Point(808, 257);
+            this.button1.Location = new System.Drawing.Point(655, 567);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(172, 47);
+            this.button1.Size = new System.Drawing.Size(140, 41);
             this.button1.TabIndex = 10;
             this.button1.Text = "Descargar Formulario";
             this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click_1);
             // 
-            // btnGraficas
+            // pictureBox2
             // 
-            this.btnGraficas.BackColor = System.Drawing.Color.BurlyWood;
-            this.btnGraficas.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
-            this.btnGraficas.Location = new System.Drawing.Point(808, 333);
-            this.btnGraficas.Name = "btnGraficas";
-            this.btnGraficas.Size = new System.Drawing.Size(172, 47);
-            this.btnGraficas.TabIndex = 11;
-            this.btnGraficas.Text = "Gráficos";
-            this.btnGraficas.UseVisualStyleBackColor = false;
-            this.btnGraficas.Click += new System.EventHandler(this.btnGraficas_Click);
+            this.pictureBox2.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.pictureBox2.Location = new System.Drawing.Point(1, -32);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(1949, 125);
+            this.pictureBox2.TabIndex = 98;
+            this.pictureBox2.TabStop = false;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToResizeColumns = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.CornflowerBlue;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(160, 121);
+            this.dataGridView1.MultiSelect = false;
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowTemplate.Height = 25;
+            this.dataGridView1.Size = new System.Drawing.Size(140, 421);
+            this.dataGridView1.TabIndex = 99;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label4.Font = new System.Drawing.Font("Segoe UI Semibold", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.label4.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label4.Location = new System.Drawing.Point(157, 96);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(143, 25);
+            this.label4.TabIndex = 100;
+            this.label4.Text = "No Verificados:";
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.pictureBox3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(1, 0);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(91, 90);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 101;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.pictureBox1.Location = new System.Drawing.Point(1, 93);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(308, 1269);
+            this.pictureBox1.TabIndex = 102;
+            this.pictureBox1.TabStop = false;
             // 
             // Paneladmin
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlLightLight;
-            this.ClientSize = new System.Drawing.Size(992, 508);
-            this.Controls.Add(this.btnGraficas);
+            this.ClientSize = new System.Drawing.Size(1149, 637);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.btnResetearPwd);
             this.Controls.Add(this.lblCorreo);
@@ -207,6 +267,8 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.dgFormularios);
             this.Controls.Add(this.dgVerificados);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Paneladmin";
@@ -214,6 +276,10 @@
             this.Load += new System.EventHandler(this.Paneladmin_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dgVerificados)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dgFormularios)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -232,6 +298,10 @@
         private System.Windows.Forms.Label lblCorreo;
         private System.Windows.Forms.Button btnResetearPwd;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button btnGraficas;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
